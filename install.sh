7c334a9a1576bedfb98874482f59b5c34355b145
Jake Rye <jrye@mit.edu>
2018-05-02 20:57:44 +0000

    making light panel physics more cohesive

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install postgresql postgresql-contrib
-sudo apt-get install python3-pip
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install postgresql -y
+sudo apt-get install postgresql-contrib -y
+sudo apt-get install python-psycopg2 -y
+sudo apt-get install libpq-dev -y
+sudo apt-get install python3-pip -y
