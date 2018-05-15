33f4699cf96cb94fb349c245c2c029f07c7ca921
Jake Rye <jrye@mit.edu>
2018-05-15 15:01:41 -0400

    wrote custom interpolate function to remove scipy dependency

--- device/peripherals/classes/peripheral.py
+++ device/peripherals/classes/peripheral.py
@@ -740,6 +740,7 @@ class Peripheral:
 
 ############################# Utility Functions ###############################
 
+    # TODO: Move these into another file..
 
     def magnitude(self, x):
         """ Gets magnitude of provided value. """
@@ -757,4 +758,47 @@ class Peripheral:
         # Get bit value
         mask = 0x1 << bit
         return (byte & mask) >> bit
+
+
+    def interpolate(self, x_list, y_list, x):
+        """ Interpolates value for x from x_list and y_list. """
+
+        # Verify x_list and y_list are same length
+        if len(x_list) != len(y_list):
+            raise ValueError("x_list and y_list must be same length")
+
+        # Verify x_list is sorted
+        if not all(x_list[i] <= x_list[i+1] for i in range(len(x_list)-1)):
+            raise ValueError("x_list must be sorted")
+
+        # Verify x in range of x_list
+        if x < x_list[0] or x > x_list[-1]:
+            raise ValueError("x is not in range of x_list")
+
+        # Check if x matches entry in x_list
+        if x in x_list:
+            index = x_list.index(x)
+            return y_list[index]
+
+        # Get index of smallest element greater than x
+        for index in range(len(x_list)):
+            if x_list[index] > x:
+                break
+        index = index - 1
+
+        # Get values for calculating slope
+        x0 = x_list[index]
+        x1 = x_list[index + 1]
+        y0 = y_list[index]
+        y1 = y_list[index + 1]
+
+        print(x0, x1, y0, y1)
+
+        # Calculate slope
+        m = (y1 - y0) / (x1 - x0)
+
+        # Calculate interpolated value and return
+        y = m * x
+        return y
+
 
