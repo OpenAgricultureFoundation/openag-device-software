bc17f4e487bab3fccc9bba5cf4476c0894767fb7
Rob Baynes <rbaynes@mit.edu>
2018-06-05 15:47:25 -0400

    automatically create django super user

--- setup_python.sh
+++ setup_python.sh
@@ -8,15 +8,18 @@ pip install -r requirements.txt
 echo 'Creating the django/postgres database...'
 python manage.py migrate
 
-echo 'Creating the django/postgres admin account:'
-python manage.py createsuperuser
+echo 'Creating the django/postgres admin account...'
+#python manage.py createsuperuser
+# The above command is interactive, the one below creates an openag / openag
+# super user account without prompting:
+echo "from django.contrib.auth.models import User; User.objects.filter(email='openag@openag.edu').delete(); User.objects.create_superuser('openag', 'openag@openag.edu', 'openag')" | python manage.py shell
 
 echo 'Creating our internal postgres application account...'
 if [[ "$OSTYPE" == "linux"* ]]; then
-  sudo -u postgres psql -c "CREATE USER openag WITH PASSWORD 'openag';"
+#  sudo -u postgres psql -c "CREATE USER openag WITH PASSWORD 'openag';"
   sudo -u postgres psql -c "CREATE DATABASE openag_brain OWNER openag;"
 else # we are on OSX
-  psql postgres -c "CREATE USER openag WITH PASSWORD 'openag';"
+#  psql postgres -c "CREATE USER openag WITH PASSWORD 'openag';"
   psql postgres -c "CREATE DATABASE openag_brain OWNER openag;"
 fi
 
