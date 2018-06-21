0c4cf427cffee9e908f541dd04b36abd7ebc7553
Demo User <debian@beaglebone.localdomain>
2018-06-21 14:50:59 +0000

    removed file cleanup and make openag user root in postgres

--- upgrade.sh
+++ upgrade.sh
@@ -12,16 +12,15 @@ echo 'Recreating database...'
 if [[ "$OSTYPE" == "linux"* ]]; then
   sudo -u postgres psql -c "DROP DATABASE openag_brain;"
   sudo -u postgres psql -c "CREATE DATABASE openag_brain OWNER openag;"
+  sudo -u postgres psql -c "ALTER USER openag SUPERUSER;"
 else # we are on OSX
   psql postgres -c "DROP DATABASE openag_brain;"
   psql postgres -c "CREATE DATABASE openag_brain OWNER openag;"
+  psql postgres -c "ALTER USER openag SUPERUSER;"
 fi
 
 # Load our models
 echo 'Creating the django/postgres database...'
 python manage.py migrate
-
-# Clean up any files we don't need
-rm logs/*.log logs/peripherals/*.log images/*.png
 
 
