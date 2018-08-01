c031c6d190973db457ea94248668d4d14e6a5381
Demo User <debian@beaglebone.localdomain>
2018-08-01 13:43:17 +0000

    added port forwarding boot script

---
+++ forward_ports.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function jsonval {
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d":" -f2 | sed -e 's/^ *//g' -e 's/ *$//g' `
+    echo ${temp##*|}
+}
+
+json=`cat about.json`
+prop='serial_number'
+serial_number=`jsonval`
+subdomain=`echo "$serial_number" | sed -r 's/[-]+/./g'`
+
+if [[ "$subdomain" = "" ]] || [[ "$subdomain" = "<DEVICE>-<VERSION>-<ID>" ]]; then
+	echo Device serial number not set, please edit about.json
+else
+	echo Forwarding ports with subdomain: $subdomain
+	autossh -M 0 -R $subdomain.serveo.net:80:localhost:8000 serveo.net -R $subdomain:22:localhost:22 serveo.net -oServerAliveInterval=30 -oStrictHostKeyChecking=no -f
+fi
--- scripts/forward_ports.sh
+++
@@ -1,6 +0,0 @@
-
-# Activate the python env for this bash process
-source ../venv/bin/activate
-
-# Run app
-python3 forward_ports.py
