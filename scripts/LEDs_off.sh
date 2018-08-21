93e79076f7c5091f0d20c5032fcc3fd00554386f
Jake Rye <jrye@mit.edu>
2018-08-21 15:45:52 +0000

    new dev cfg

--- scripts/LEDs_off.sh
+++ scripts/LEDs_off.sh
@@ -2,4 +2,4 @@
 cd ~/openag-device-software
 source venv/bin/activate
 cd device/peripherals/modules/led_dac5578
-python scripts/run_panel.py --device edu002 --off
+python scripts/run_panel.py --device edu-v0.3.0 --off
