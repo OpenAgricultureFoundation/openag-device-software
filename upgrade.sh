5120194ec05f70c8e8b4ad4523587ccde77840ec
rob <rbaynes@gmail.com>
2018-08-28 15:16:02 +0000

    wip

--- upgrade.sh
+++ upgrade.sh
@@ -16,19 +16,27 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 # Recreate a fresh (empty) database
-echo 'Recreating database...'
+#echo 'Recreating database...'
+#if [[ "$OSTYPE" == "linux"* ]]; then
+#  sudo -u postgres psql -c "DROP DATABASE openag_brain;"
+#  sudo -u postgres psql -c "CREATE DATABASE openag_brain OWNER openag;"
+#  sudo -u postgres psql -c "ALTER USER openag SUPERUSER;"
+#else # we are on OSX
+#  psql postgres -c "DROP DATABASE openag_brain;"
+#  psql postgres -c "CREATE DATABASE openag_brain OWNER openag;"
+#  psql postgres -c "ALTER USER openag SUPERUSER;"
+#fi
+
+# Remove all rows in the state table only.
+echo 'Updating database...'
 if [[ "$OSTYPE" == "linux"* ]]; then
-  sudo -u postgres psql -c "DROP DATABASE openag_brain;"
-  sudo -u postgres psql -c "CREATE DATABASE openag_brain OWNER openag;"
-  sudo -u postgres psql -c "ALTER USER openag SUPERUSER;"
+  sudo -u postgres psql openag_brain -c "DELETE FROM app_statemodel;"
 else # we are on OSX
-  psql postgres -c "DROP DATABASE openag_brain;"
-  psql postgres -c "CREATE DATABASE openag_brain OWNER openag;"
-  psql postgres -c "ALTER USER openag SUPERUSER;"
+  psql postgres openag_brain -c "DELETE FROM app_statemodel;"
 fi
 
 # Load our models
-echo 'Creating the django/postgres database...'
+echo 'Migrating the django/postgres database...'
 python manage.py migrate
 
 # Also need to make our user super again for the django admin
@@ -38,13 +46,12 @@ echo "from django.contrib.auth.models import User; User.objects.filter(email='op
 # openssl s_client -connect mqtt.googleapis.com:8883
 
 # Check if brain root env is exported in venv activate, if not add it
-cwd=`pwd`
-if ! grep "OPENAG_BRAIN_ROOT" $cwd/venv/bin/activate > /dev/null; then
-   echo "export OPENAG_BRAIN_ROOT=$cwd" >> $cwd/venv/bin/activate
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if ! grep "OPENAG_BRAIN_ROOT" $DIR/venv/bin/activate > /dev/null; then
+   echo "export OPENAG_BRAIN_ROOT=$DIR" >> $DIR/venv/bin/activate
 fi
 
-# Remove rc.local and sym link to openag-device-software/config/rc.local
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Remove rc.local and sym link to config/rc.local
 sudo rm -f /etc/rc.local
 sudo ln -s $DIR/config/rc.local /etc/rc.local
 sudo systemctl daemon-reload
