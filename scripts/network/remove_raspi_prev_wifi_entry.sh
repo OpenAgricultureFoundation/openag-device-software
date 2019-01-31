32352cb05588f2dd11d3a8cf310a0f1cabfb2a1e
Jake Rye <jrye@mit.edu>
2019-01-31 14:38:37 -0500

    fixed remove prev raspi wifi entry script

--- scripts/network/remove_raspi_prev_wifi_entry.sh
+++ scripts/network/remove_raspi_prev_wifi_entry.sh
@@ -2,7 +2,9 @@
 
 NUM_LINES=`wc -l < /etc/wpa_supplicant/wpa_supplicant.conf`
 
-if [[ $NUM_LINES -gt 9 ]]; then
+if [[ $NUM_LINES -gt 7 ]]; then
 	head -n -6 /etc/wpa_supplicant/wpa_supplicant.conf > /etc/wpa_supplicant/tmp
 	cp /etc/wpa_supplicant/tmp /etc/wpa_supplicant/wpa_supplicant.conf
+else
+	exit 1
 fi
