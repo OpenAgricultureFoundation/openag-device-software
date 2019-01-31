56dbf5c0765cad56f3bbc2ee80ca6b39e56ac2e8
Jake Rye <jrye@mit.edu>
2019-01-31 15:08:41 -0500

    fixed check for platform variable definition

--- scripts/network/restart_network_connection.sh
+++ scripts/network/restart_network_connection.sh
@@ -14,13 +14,13 @@ fi
 PROJECT_ROOT=$1
 
 # Ensure platform is defined
-if [[ ! -z "$PLATFORM" ]]; then
+if [[ -z "$PLATFORM" ]]; then
 	source $PROJECT_ROOT/scripts/platform/get_platform_info.sh
 	echo $PLATFORM
 fi
 
 # Check if platform still not defined
-if [[ ! -z "$PLATFORM" ]]; then
+if [[ -z "$PLATFORM" ]]; then
 	echo "Unable to restart network, platform is not defined"
 	exit 1
 fi
