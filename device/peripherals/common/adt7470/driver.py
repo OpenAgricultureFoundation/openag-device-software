d1e7d2b4022e6728f7c17db563c0db2a9e281676
Jake Rye <jrye@mit.edu>
2019-07-24 11:40:38 -0400

    adds driver functions for configuring automatic fan control

--- device/peripherals/common/adt7470/driver.py
+++ device/peripherals/common/adt7470/driver.py
@@ -14,19 +14,29 @@ from device.utilities.communication.i2c.mux_simulator import MuxSimulator
 from device.peripherals.common.pca9633.simulator import PCA9633Simulator
 from device.peripherals.common.pca9633 import exceptions
 
-# Initialize registers
+# Define identification registers
 VERSION_REGISTER = 0x3F
+
+# Define config registers
 CONFIG_REGISTER_1 = 0x40
 CONFIG_REGISTER_2 = 0x74
-TEMPERATURE_BASE_REGISTER = 0x20
-MAX_TEMPERATURE_REGISTER = 0x78
-TEMPERATURE_LIMIT_BASE_REGISTER = 0x44
-TACHOMETER_BASE_REGISTER = 0x2A
-CURRENT_DUTY_CYCLE_BASE_REGISTER = 0x32
-MIN_DUTY_CYCLE_BASE_REGISTER = 0x6A
-MAX_DUTY_CYCLE_BASE_REGISTER = 0x38
 
-# Initialize masks
+# Define temperature registers
+TEMPERATURE_BASE_REGISTER = 0x20
+MAXIMUM_TEMPERATURE_REGISTER = 0x78
+
+# Define fan control registers
+PWM_CONFIG_BASE_REGISTER = 0x68
+PWM_MINIMUM_DUTY_CYCLE_BASE_REGISTER = 0x6A
+PWM_MAXIMUM_DUTY_CYCLE_BASE_REGISTER = 0x38
+PWM_CURRENT_DUTY_CYCLE_BASE_REGISTER = 0x32
+THERMAL_ZONE_CONFIG_BASE_REGISTER = 0x7C
+THERMAL_ZONE_MINIMUM_TEMPERATURE_BASE_REGISTER = 0x6E
+
+# Define fan feedback registers
+TACHOMETER_BASE_REGISTER = 0x2A
+
+# Define masks
 ENABLE_MONITORING_MASK = 0x80
 DISABLE_MONITORING_MASK = 0x7F
 SHUTDOWN_MASK = 0x01
@@ -87,8 +97,6 @@ class ADT7470Driver:
 
         # Lock thread in case we have multiple io expander instances
         with self.i2c_lock:
-
-            # Send set output command to ic
             try:
                 version = self.i2c.read_register(VERSION_REGISTER)
                 self.logger.debug("Version: {}".format(version))
@@ -96,21 +104,185 @@ class ADT7470Driver:
             except I2CError as e:
                 raise exceptions.ReadVersionError(logger=self.logger) from e
 
-    # def enable_automatic_fan_control(self, fan_id: int) -> None:
-    #     """Enables automatic fan control."""
-    #     self.logger.debug("Enabling automatic fan control for fan {}".format(fan_id))
+    def enable_manual_fan_control(self, fan_id: int) -> None:
+        """Enables manual fan control."""
+        self.logger.debug("Enabling manual fan control for fan {}".format(fan_id))
 
-    #     # Lock thread in case we have multiple io expander instances
-    #     with self.i2c_lock:
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
 
-    #         # Send set output command to ic
-    #         try:
-    #             low_address = PWM_BASE_REGISTER + 2 * fan_id
-    #             high_address = low_address + 1
-    #             low_byte = self.i2c.read_register(low_address)
-    #             high_byte = self.i2c.read_register(high_address)
-    #         except I2CError as e:
-    #             raise exceptions.ReadTachometerError(logger=self.logger) from e
+        # Lock thread in case we have multiple io expander instances
+        with self.i2c_lock:
+            try:
+                register_address = PWM_CONFIG_BASE_REGISTER + int(fan_id / 2)
+                register_byte = self.i2c.read_register(register_address)
+                register_byte &= 255 - (1 << 7 - fan_id % 2)
+                self.i2c.write_register(register_address, register_byte)
+            except I2CError as e:
+                raise exceptions.EnableManualFanControlError(logger=self.logger) from e
+
+    def enable_automatic_fan_control(self, fan_id: int) -> None:
+        """Enables automatic fan control."""
+        self.logger.debug("Enabling automatic fan control for fan {}".format(fan_id))
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Lock thread in case we have multiple io expander instances
+        with self.i2c_lock:
+            try:
+                register_address = PWM_CONFIG_BASE_REGISTER + int(fan_id / 2)
+                register_byte = self.i2c.read_register(register_address)
+                register_byte |= 1 << 7 - fan_id % 2
+                self.i2c.write_register(register_address, register_byte)
+            except I2CError as e:
+                raise exceptions.EnableAutomaticFanControlError(
+                    logger=self.logger
+                ) from e
+
+    def write_thermal_zone_config(self, fan_id: int, control_sensor_id) -> None:
+        """Writes thermal zone for fan id to be controlled by sensor id."""
+        self.logger.debug("Writing thermal zone config")
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Check if control sensor is enabled
+        if control_sensor_id is None:
+            return
+
+        # Get register nibble
+        if type(control_sensor_id) is str and control_sensor_id == "max":
+            register_nibble = 0
+        elif type(
+            control_sensor_id
+        ) is int and control_sensor_id >= 0 and control_sensor_id <= 9:
+            register_nibble = control_sensor_id + 1
+        else:
+            raise ValueError("Control sensor id must be 'max', 0-9, or null")
+
+        # Write thermal zone
+        with self.i2c_lock:
+            try:
+                register_address = THERMAL_ZONE_CONFIG_BASE_REGISTER + int(fan_id / 2)
+                register_byte = self.i2c.read_register(register_address)
+                # self.logger.debug("init register_byte: {}".format(hex(register_byte)))
+                nibble_index = ((fan_id + 1) % 2) * 4
+                # self.logger.debug("nibble_index: {}".format(nibble_index))
+                register_byte &= (0xF << (4 - nibble_index))  # Clear register nibble
+                # self.logger.debug("clear register_byte: {}".format(hex(register_byte)))
+                register_byte += (register_nibble << nibble_index)
+                self.i2c.write_register(register_address, register_byte)
+            except I2CError as e:
+                raise exceptions.WriteThermalZoneConfigError(logger=self.logger) from e
+
+    def write_thermal_zone_minimum_temperature(
+        self, fan_id: int, minimum_temperature_celsius: float
+    ):
+        """Writes the minimum temperature for a fan."""
+        self.logger.debug("Writing minimum temperature for fan {}".format(fan_id))
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Validate minimum temperature celcius
+        if minimum_temperature_celsius < -127 or minimum_temperature_celsius > 127:
+            raise ValueError("Minimum temperature must be a value between -127 to 127")
+
+        # Convert temperature float to int
+        temperature_int = int(minimum_temperature_celsius)
+
+        # Convert temperature int to byte
+        if temperature_int < 0:
+            temperature_byte = 128 - temperature_int
+        else:
+            temperature_byte = temperature_int
+        self.logger.debug("temperature_byte: {}".format(temperature_byte))
+
+        # Write minimum temperature
+        # with self.i2c_lock:
+        #     try:
+        #         register_address = THERMAL_ZONE_MINIMUM_TEMPERATURE_BASE_REGISTER + fan_id
+        #         self.i2c.write_register(register_address, temperature_byte)
+        #     except I2CError as e:
+        #         raise exceptions.WriteThermalZoneMinimumTemperature(
+        #             logger=self.logger
+        #         ) from e
+
+    def write_minimum_duty_cycle(self, fan_id: int, duty_cycle: float):
+        """Writes minimum pwm duty cycle for a fan."""
+        self.logger.debug("Writing minimum duty cycle for fan {}".format(fan_id))
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Validate duty cycle
+        if duty_cycle < 0 or duty_cycle > 100:
+            raise ValueError("Duty cycle must be a value between 0-100")
+
+        # Convert duty cycle from float to byte
+        duty_cycle_byte = int(duty_cycle / 0.39)
+
+        # Write minimum duty cycle
+        with self.i2c_lock:
+            try:
+                address = PWM_MINIMUM_DUTY_CYCLE_BASE_REGISTER + fan_id
+                self.i2c.write_register(address, duty_cycle_byte)
+            except I2CError as e:
+                raise exceptions.WriteMinDutyCycleError(logger=self.logger) from e
+
+    def write_maximum_duty_cycle(self, fan_id: int, duty_cycle: float):
+        """Writes max pwm duty cycle for a fan."""
+        self.logger.debug("Writing maximum duty cycle for fan {}".format(fan_id))
+
+        # Validate duty cycle
+        if duty_cycle < 0 or duty_cycle > 100:
+            raise ValueError("Duty cycle must be a value between 0-100")
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Convert duty cycle from float to int
+        duty_cycle_byte = int(duty_cycle / 0.39)
+
+        # Write maximum duty cycle
+        with self.i2c_lock:
+            try:
+                address = PWM_MAXIMUM_DUTY_CYCLE_BASE_REGISTER + fan_id
+                self.i2c.write_register(address, duty_cycle_byte)
+            except I2CError as e:
+                raise exceptions.WriteMaxDutyCycleError(logger=self.logger) from e
+
+    def read_current_duty_cycle(self, fan_id: int) -> float:
+        """Reads a fan pwm."""
+        self.logger.debug("Reading curent duty cycle for fan {}".format(fan_id))
+
+        # Validate fan id
+        if fan_id < 0 or fan_id > 3:
+            raise ValueError("Fan id must be a value between 0-3")
+
+        # Lock thread in case we have multiple io expander instances
+        with self.i2c_lock:
+
+            # Send set output command to ic
+            try:
+                low_address = PWM_CURRENT_DUTY_CYCLE_BASE_REGISTER + fan_id
+                register_byte = self.i2c.read_register(low_address)
+            except I2CError as e:
+                raise exceptions.ReadCurrentDutyCycleError(logger=self.logger) from e
+
+        # Convert register byte to duty cycle
+        duty_cycle = round(0.39 * register_byte, 2)
+
+        # Return duty cycle
+        self.logger.debug("Duty cycle: {}%".format(duty_cycle))
+        return duty_cycle
 
     def enable_monitoring(self, retry: bool = True) -> None:
         """Enables temperature monitoring."""
@@ -118,8 +290,6 @@ class ADT7470Driver:
 
         # Lock thread in case we have multiple io expander instances
         with self.i2c_lock:
-
-            # Send set output command to ic
             try:
                 register_byte = self.i2c.read_register(CONFIG_REGISTER_1)
                 register_byte |= ENABLE_MONITORING_MASK
@@ -133,8 +303,6 @@ class ADT7470Driver:
 
         # Lock thread in case we have multiple io expander instances
         with self.i2c_lock:
-
-            # Send set output command to ic
             try:
                 register_byte = self.i2c.read_register(CONFIG_REGISTER_1)
                 register_byte &= DISABLE_MONITORING_MASK
@@ -199,7 +367,7 @@ class ADT7470Driver:
 
             # Send set output command to ic
             try:
-                max_temperature = self.i2c.read_register(MAX_TEMPERATURE_REGISTER)
+                max_temperature = self.i2c.read_register(MAXIMUM_TEMPERATURE_REGISTER)
                 self.logger.debug("Max Temperature: {}".format(max_temperature))
                 return max_temperature
             except I2CError as e:
@@ -220,78 +388,3 @@ class ADT7470Driver:
                 high_byte = self.i2c.read_register(high_address)
             except I2CError as e:
                 raise exceptions.ReadTachometerError(logger=self.logger) from e
-
-    def read_current_duty_cycle(self, fan_id: int) -> float:
-        """Reads a fan pwm."""
-        self.logger.debug("Reading curent duty cycle for fan {}".format(fan_id))
-
-        # Validate fan id
-        if fan_id < 0 or fan_id > 3:
-            raise ValueError("Fan id must be a value between 0-3")
-
-        # Lock thread in case we have multiple io expander instances
-        with self.i2c_lock:
-
-            # Send set output command to ic
-            try:
-                low_address = CURRENT_DUTY_CYCLE_BASE_REGISTER + fan_id
-                register_byte = self.i2c.read_register(low_address)
-            except I2CError as e:
-                raise exceptions.ReadCurrentDutyCycleError(logger=self.logger) from e
-
-        # Convert register byte to duty cycle
-        duty_cycle = round(0.39 * register_byte, 2)
-
-        # Return duty cycle
-        self.logger.debug("Duty cycle: {}%".format(duty_cycle))
-        return duty_cycle
-
-    def write_min_duty_cycle(self, fan_id: int, duty_cycle: float):
-        """Writes max pwm duty cycle."""
-        self.logger.debug("Writing min duty cycle for fan {}".format(fan_id))
-
-        # Validate duty cycle
-        if duty_cycle < 0 or duty_cycle > 100:
-            raise ValueError("Duty cycle must be a value between 0-100")
-
-        # Validate fan id
-        if fan_id < 0 or fan_id > 3:
-            raise ValueError("Fan id must be a value between 0-3")
-
-        # Convert duty cycle
-        duty_cycle_byte = int(duty_cycle / 0.39)
-
-        # Lock thread in case we have multiple io expander instances
-        with self.i2c_lock:
-
-            # Write to driver
-            try:
-                address = MIN_DUTY_CYCLE_BASE_REGISTER + fan_id
-                self.i2c.write_register(address, duty_cycle_byte)
-            except I2CError as e:
-                raise exceptions.WriteMinDutyCycleError(logger=self.logger) from e
-
-    def write_max_duty_cycle(self, fan_id: int, duty_cycle: float):
-        """Writes max pwm duty cycle."""
-        self.logger.debug("Writing max duty cycle for fan {}".format(fan_id))
-
-        # Validate duty cycle
-        if duty_cycle < 0 or duty_cycle > 100:
-            raise ValueError("Duty cycle must be a value between 0-100")
-
-        # Validate fan id
-        if fan_id < 0 or fan_id > 3:
-            raise ValueError("Fan id must be a value between 0-3")
-
-        # Convert duty cycle
-        duty_cycle_byte = int(duty_cycle / 0.39)
-
-        # Lock thread in case we have multiple io expander instances
-        with self.i2c_lock:
-
-            # Write to driver
-            try:
-                address = MAX_DUTY_CYCLE_BASE_REGISTER + fan_id
-                self.i2c.write_register(address, duty_cycle_byte)
-            except I2CError as e:
-                raise exceptions.WriteMaxDutyCycleError(logger=self.logger) from e
