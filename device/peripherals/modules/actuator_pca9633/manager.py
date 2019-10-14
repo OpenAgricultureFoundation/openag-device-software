57e5fdd5811c31986736730a4991e7d4379ebd78
Jake Rye <jrye@mit.edu>
2019-10-14 16:50:15 +0100

    indicator leds have a 60 second heartbeat

--- device/peripherals/modules/actuator_pca9633/manager.py
+++ device/peripherals/modules/actuator_pca9633/manager.py
@@ -22,11 +22,14 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
         super().__init__(*args, **kwargs)
 
         # Initialize colors
-        self.colors = ["Red", "Green", "Yellow", "Blue"]
+        self.colors = ["Red", "Green", "Yellow", "Blue", "Off"]
 
-        # Set default sampling interval
-        self.default_sampling_interval = 3  # second
+        # Set defaults
+        self.default_sampling_interval = 3  # seconds
         self.prev_update = 0  # timestamp
+        self.first_user_led_update = True
+        self.previous_heartbeat_timestamp = 0
+        self.heartbeat_interval = 60 # seconds
 
     @property
     def network_is_connected(self) -> bool:
@@ -120,7 +123,6 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
     @user_indicator_led_status.setter
     def user_indicator_led_status(self, value: bool) -> None:
         """Sets reported output value in shared state."""
-        self.logger.debug("Setting user indicator: {}".format(value))
         self.state.set_peripheral_reported_actuator_value(
             self.name, "user_indicator_led_status", value
         )
@@ -197,22 +199,38 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
         """Sets up peripheral."""
         self.logger.debug("No setup required for peripheral")
 
-        # Turn off all indicator leds
+        # Turn off leds
         for driver in self.drivers:
             driver.set_rgb([0, 0, 0])
+        
+        # Update indicator status
+        self.peripheral_indicator_led_status = "Off"
+        self.network_indicator_led_status = "Off"
+        self.iot_indicator_led_status = "Off"
+        self.user_indicator_led_status = "Off"
 
     def update_peripheral(self) -> None:
         """Updates peripheral by setting output to desired state."""
+
+        # Check for heartbeat
+        send_heartbeat = False
+        current_timestamp = time.time()
+        if current_timestamp - self.previous_heartbeat_timestamp > self.heartbeat_interval:
+            self.previous_heartbeat_timestamp = current_timestamp
+            send_heartbeat = True
+            self.logger.debug("Sending heartbeat")
+
+        # Update each driver
         try:
             for driver in self.drivers:
                 if driver.name == "NetworkLED":
-                    self.update_network_led(driver)
+                    self.update_network_led(driver, send_heartbeat)
                 elif driver.name == "IoTLED":
-                    self.update_iot_led(driver)
+                    self.update_iot_led(driver, send_heartbeat)
                 elif driver.name == "PeripheralLED":
-                    self.update_peripheral_led(driver)
+                    self.update_peripheral_led(driver, send_heartbeat)
                 elif driver.name == "UserLED":
-                    self.update_user_led(driver)
+                    self.update_user_led(driver, send_heartbeat)
         except exceptions.DriverError as e:
             self.logger.exception("Unable to update peripheral: {}".format(e))
             self.mode = modes.ERROR
@@ -253,44 +271,34 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
 
     ##### HELPER METHODS ###############################################################
 
-    def update_network_led(self, driver: driver.PCA9633Driver) -> None:
+    def update_network_led(self, driver: driver.PCA9633Driver, send_heartbeat: bool = False) -> None:
         """Updates network indicator and status."""
         if self.network_is_connected:
-            if self.network_indicator_led_status != "Green":
+            if self.network_indicator_led_status != "Green" or send_heartbeat:
+                self.logger.debug("Setting network led green")
                 driver.set_rgb([0, 32, 0])
                 self.network_indicator_led_status = "Green"
         else:
-            if self.network_indicator_led_status != "Red":
+            if self.network_indicator_led_status != "Red" or send_heartbeat:
+                self.logger.debug("Setting network led red")
                 driver.set_rgb([32, 0, 0])
                 self.network_indicator_led_status = "Red"
 
-    def update_iot_led(self, driver: driver.PCA9633Driver) -> None:
+    def update_iot_led(self, driver: driver.PCA9633Driver, send_heartbeat: bool = False) -> None:
         """Updates iot indicator and status."""
         if self.iot_is_connected:
-            if self.iot_indicator_led_status != "Green":
+            if self.iot_indicator_led_status != "Green" or send_heartbeat:
+                self.logger.debug("Setting iot led green")
                 driver.set_rgb([0, 32, 0])
                 self.iot_indicator_led_status = "Green"
         else:
-            if self.iot_indicator_led_status != "Red":
+            if self.iot_indicator_led_status != "Red" or send_heartbeat:
+                self.logger.debug("Setting iot led red")
                 driver.set_rgb([32, 0, 0])
                 self.iot_indicator_led_status = "Red"
 
-    def update_iot_and_network_led(self, driver: driver.PCA9633Driver) -> None:
-        """Updates iot/network indicator and status."""
-        if self.iot_is_connected:
-            if self.iot_indicator_led_status != "Green":
-                driver.set_rgb([0, 32, 0])
-                self.iot_indicator_led_status = "Green"
-        elif self.network_is_connected:
-            if self.iot_indicator_led_status != "Yellow":
-                driver.set_rgb([32, 32, 0])
-                self.iot_indicator_led_status = "Yellow"
-        else:
-            if self.iot_indicator_led_status != "Red":
-                driver.set_rgb([32, 0, 0])
-                self.iot_indicator_led_status = "Red"
 
-    def update_peripheral_led(self, driver: driver.PCA9633Driver) -> None:
+    def update_peripheral_led(self, driver: driver.PCA9633Driver, send_heartbeat: bool = False) -> None:
         """Updates peripheral led indicator and status."""
 
         # Check if all peripherals are setup and healthy
@@ -311,24 +319,49 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
 
         # Update indicator led output
         if not setup:
-            if self.peripheral_indicator_led_status != "Yellow":
+            if self.peripheral_indicator_led_status != "Yellow" or send_heartbeat:
+                self.logger.debug("Setting peripheral led yellow")
                 driver.set_rgb([32, 32, 0])
                 self.peripheral_indicator_led_status = "Yellow"
         elif healthy:
-            if self.peripheral_indicator_led_status != "Green":
+            if self.peripheral_indicator_led_status != "Green" or send_heartbeat:
+                self.logger.debug("Setting peripheral led green")
                 driver.set_rgb([0, 32, 0])
                 self.peripheral_indicator_led_status = "Green"
         else:
-            if self.peripheral_indicator_led_status != "Red":
+            if self.peripheral_indicator_led_status != "Red" or send_heartbeat:
+                self.logger.debug("Setting peripheral led red")
                 driver.set_rgb([32, 0, 0])
                 self.peripheral_indicator_led_status = "Red"
 
-    def update_user_led(self, driver: driver.PCA9633Driver) -> None:
+    def update_user_led(self, driver: driver.PCA9633Driver, send_heartbeat: bool = False) -> None:
         """Updates user led indicator and status."""
-        ...
-        # if self.user_indicator_led_status != "Green":
-        #     driver.set_rgb([0, 32, 0])
-        #     self.user_indicator_led_status = "Green"
+
+        # Set green on first update
+        if self.first_user_led_update:
+            self.first_user_led_update = False
+            self.logger.debug("Setting user led green")
+            driver.set_rgb([0, 32, 0])
+            self.user_indicator_led_status = "Green"
+
+        # Check to send heartbeat
+        elif send_heartbeat:
+            if self.user_indicator_led_status == "Green": 
+                self.logger.debug("Setting user led green")
+                driver.set_rgb([0, 32, 0])
+            elif self.user_indicator_led_status == "Yellow":
+                self.logger.debug("Setting user led yellow")
+                driver.set_rgb([32, 32, 0])
+            elif self.user_indicator_led_status == "Red":
+                self.logger.debug("Setting user led red")
+                driver.set_rgb([32, 0, 0])
+            elif self.user_indicator_led_status == "Blue":
+                self.logger.debug("Setting user led blue")
+                driver.set_rgb([0, 0, 32])
+            elif self.user_indicator_led_status == "Off":
+                self.logger.debug("Setting user led off")
+                driver.set_rgb([0, 0, 0])
+
 
  ##### EVENT FUNCTIONS ##############################################################
 
@@ -358,7 +391,7 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
 
         # Validate parameters
         if color not in self.colors:
-          return "Invalid color, must be Red, Green, Blue, or Yellow", 400
+          return "Invalid color, must be Red, Green, Blue, Yellow, or Off", 400
 
         # Add event request to event queue
         request = {"type": events.SET_USER_LED, "color": color}
@@ -380,17 +413,20 @@ class ActuatorPCA9633Manager(manager.PeripheralManager):
             for driver in self.drivers:
                 if driver.name == "UserLED":
                     if color == "Green": 
-                        self.logger.debug("Setting color green")
+                        self.logger.debug("Setting user led green")
                         driver.set_rgb([0, 32, 0])
                     elif color == "Yellow":
-                        self.logger.debug("Setting color yellow")
+                        self.logger.debug("Setting user led yellow")
                         driver.set_rgb([32, 32, 0])
                     elif color == "Red":
-                        self.logger.debug("Setting color red")
+                        self.logger.debug("Setting user led red")
                         driver.set_rgb([32, 0, 0])
                     elif color == "Blue":
-                        self.logger.debug("Setting color blue")
+                        self.logger.debug("Setting user led blue")
                         driver.set_rgb([0, 0, 32])
+                    elif color == "Off":
+                        self.logger.debug("Setting user led blue")
+                        driver.set_rgb([0, 0, 0])
                     break
             
         except exceptions.DriverError as e:
